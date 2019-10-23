bbea5fcc046b91060ed80692e6a6068c52057cee
Günther Enthaler <genthaler@users.noreply.github.com>
2019-10-24 07:50:12 +1100

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -1,41 +1,55 @@
 module Main exposing (main)
 
 import Browser
-import Element exposing (Element, alignRight, centerX,centerY, column, el, fill, layout, padding, rgb255, row, spacing, text, width)
+import Debug exposing (todo)
+import Element exposing (Element, alignRight, centerX, centerY, column, el, explain, fill, height, layout, padding, rgb255, row, spaceEvenly, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
-import List
 import Html exposing (Html)
+import List
 
-type alias Model = List String
-type alias Msg = ()
 
+type alias Model =
+    List (List String)
+
+
+type alias Msg =
+    ()
+
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-    ( ["X","X","X","X","X","X","X","X","X"], Cmd.none )
+    ( [ [ "X", "X", "X" ], [ "X", "X", "X" ], [ "X", "X", "X" ] ], Cmd.none )
+
+
+debug =
+    Element.explain Debug.todo
+
 
 view : Model -> Html Msg
 view model =
-    layout [ Background.color (rgb255 200 200 200),centerX, centerY ] <|
-        column [centerX, centerY] [
-            row [centerX, centerY] [
-                text "X",
-                text "X",
-                text "X"
-            ],
-            row [centerX, centerY] [
-                text "X",
-                text "X",
-                text "X"
-            ],
-            row [centerX, centerY] [
-                text "X",
-                text "X",
-                text "X"
-            ]
-        ]
+    let
+        myRow : List String -> Element Msg
+        myRow =
+            row [ spacing 20, debug, width fill, height fill, spaceEvenly ]
+                << List.map (el [] << text)
+
+        myCol : List (List String) -> Element Msg
+        myCol =
+            column [ padding 100, spacing 20, debug, width fill, height fill, spaceEvenly ]
+                << List.map myRow
+    in
+    layout [ Background.color (rgb255 200 200 200), centerX, centerY ]
+        << column [ padding 100, spacing 20, debug, width fill, height fill, spaceEvenly ]
+        << List.map
+            row
+            [ spacing 20, debug, width fill, height fill, spaceEvenly ]
+        << List.map (el [] << text)
 
 
+
+-- myCol model
 
 
 update _ model =
