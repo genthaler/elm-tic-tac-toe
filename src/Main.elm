5ac3ba2bda079b1846ed789c5d7847af0af26e62
Günther Enthaler <genthaler@users.noreply.github.com>
2019-10-23 17:14:12 +1100

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -1,36 +1,51 @@
 module Main exposing (main)
 
 import Browser
-import Element exposing (Element, alignRight, centerY, column, el, fill, layout, padding, rgb255, row, spacing, text, width)
+import Element exposing (Element, alignRight, centerX,centerY, column, el, fill, layout, padding, rgb255, row, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
+import List
+import Html exposing (Html)
 
+type alias Model = List String
+type alias Msg = ()
 
 init _ =
-    ( (), Cmd.none )
+    ( ["X","X","X","X","X","X","X","X","X"], Cmd.none )
 
-
-view _ =
-    layout [ Background.color (rgb255 111 111 111) ] <|
-        el
-            [ Background.color (rgb255 111 111 111)
-            , Font.color (rgb255 255 255 255)
-            , Border.rounded 3
-            , padding 30
+view : Model -> Html Msg
+view model =
+    layout [ Background.color (rgb255 200 200 200),centerX, centerY ] <|
+        column [centerX, centerY] [
+            row [centerX, centerY] [
+                text "X",
+                text "X",
+                text "X"
+            ],
+            row [centerX, centerY] [
+                text "X",
+                text "X",
+                text "X"
+            ],
+            row [centerX, centerY] [
+                text "X",
+                text "X",
+                text "X"
             ]
-        <|
-            text "stylish!"
+        ]
 
 
-update _ _ =
-    ( (), Cmd.none )
+
+
+update _ model =
+    ( model, Cmd.none )
 
 
 subscriptions _ =
     Sub.none
 
 
-main : Program () () msg
+main : Program () Model Msg
 main =
     Browser.element { init = init, view = view, update = update, subscriptions = subscriptions }
