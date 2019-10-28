4c57fb7b48533b7f7ad04ec6762458e2f3a10864
Günther Enthaler <genthaler@users.noreply.github.com>
2019-10-28 18:17:11 +1100

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -2,15 +2,13 @@ module Main exposing (main)
 
 import Browser
 import Debug exposing (todo)
-import Element exposing (Element, alignRight, centerX, centerY, column, el, explain, fill, height, layout, none, padding, paddingXY, px, rgb255, row, spaceEvenly, spacing, text, width)
+import Element exposing (Element, alignRight, centerX, centerY, column, el, explain, fill, height, layout, none, padding, paddingXY, px, rgb255, rgb, row, spaceEvenly, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
+import Element.Input as Input
 import Html exposing (Html)
 import List
-import Svg
-import Svg.Attributes
-
 
 type alias Model =
     List (List String)
@@ -22,37 +20,22 @@ type alias Msg =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    -- ( [ [ "X", "X", "X" ], [ "X", "X", "X" ], [ "X", "X", "X" ] ], Cmd.none )
-    ( [ [ "X" ] ], Cmd.none )
+    ( [ [ "X", "X", "X" ], [ "X", "X", "X" ], [ "X", "X", "X" ] ], Cmd.none )
+    -- ( [ [ "X" ] ], Cmd.none )
 
 
 debug =
     Element.explain Debug.todo
 
 
-svgX : String -> Element msg
-svgX _ =
-    Svg.svg
-        [ Svg.Attributes.style "background: white"
-        , Svg.Attributes.height "100%"
-        , Svg.Attributes.width "100%"
-        ]
-        [ Svg.
-            [ Svg.Attributes.r "50%"
-            ]
-            []
-        ]
-        |> Element.html
-
-
 myCell : String -> Element msg
 myCell s =
-    column [ width fill, height fill, debug ] [ row [ width fill, height fill, debug ] [ none, none ], row [ width fill, height fill, debug ] [ none, svgX s ] ]
+    column [ width fill, height fill ] [ row [ width fill, height fill ] [ none, none ], row [ width fill, height fill ] [ none, Input.button [width fill, height fill, Border.color (rgb 0 0.7 0), Border.solid] {onPress=Nothing, label= el [centerX, centerY] <|text s} ] ]
 
 
 myRow : List String -> Element Msg
 myRow =
-    row [ width fill, height fill, debug ]
+    row [ width fill, height fill ]
         << List.map (el [ width fill, height fill ] << myCell)
 
 
